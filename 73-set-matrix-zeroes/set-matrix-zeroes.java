746ce9d31abea94159c6d6fb2d13b7be27300369
Nitish kumar <nitishdawas@gmail.com>
2026-06-27 00:37:08 +0530

    Time: 3 ms (20.51%) | Memory: 47.3 MB (95.23%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        HashSet<Integer> r = new HashSet<>();
+        HashSet<Integer> c = new HashSet<>();
+        for(int i = 0; i <n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                     r.add(i);
+                    c.add(j);
+                }   
+                }
+            }
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(r.contains(i) || c.contains(j)){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        
+    }
+}
